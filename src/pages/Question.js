98e998301f9ef597ebd95751a5b3a870010e41e4
joao-rafael <joaorafa131@gmail.com>
2020-10-12 23:34:04 -0300

    feat: added question layout

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -7,16 +7,60 @@ const Home = () => {
     return (
         <QuestionLayout>
 
-            <section className='section -home'>
+            <section className='section -question'>
                 <Container>
                     <Row>
                         <Col lg={12} className='s-col'>
+                           <h2 className='title -sub'>
+                                Questão 1
+                           </h2>
                            <h1 className='title -question'>
                                FizzBuzz
                            </h1>
-                           <p>
-                               
+                           <p className='s-paragraph'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Donec eget sem id felis egestas ullamcorper. 
+                                Proin eu eros quis arcu vestibulum posuere. 
+                                Maecenas pellentesque odio turpis, non varius 
+                                ligula efficitur pellentesque. Maecenas molestie 
+                                felis at dui pulvinar sodales. 
+                                Integer sagittis tellus a sem elementum, 
+                                et semper nisi sollicitudin. Etiam pretium ante 
+                                et scelerisque ornare. Vestibulum rhoncus justo in 
+                                ligula porta porttitor. 
                            </p>
+                           <pre className='s-code'>
+                                lorem 
+                            </pre> 
+                            <p className='s-paragraph'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Donec eget sem id felis egestas ullamcorper. 
+                                Proin eu eros quis arcu vestibulum posuere. 
+                                Maecenas pellentesque odio turpis, non varius 
+                                ligula efficitur pellentesque. Maecenas molestie 
+                                felis at dui pulvinar sodales. 
+                                Integer sagittis tellus a sem elementum, 
+                                et semper nisi sollicitudin. Etiam pretium ante 
+                                et scelerisque ornare. Vestibulum rhoncus justo in 
+                                ligula porta porttitor. 
+                           </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div className='sendzone'>
+                                <h2 className='s-title'>
+                                    Área de envio:
+                                </h2>
+                                <div className='s-box'>
+                                    <p>
+                                        arraste sua submissão aqui
+                                    </p>
+                                </div>
+                                <a>
+                                    voltar
+                                </a>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
